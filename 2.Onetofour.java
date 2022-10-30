03b567425cd87fda24fed4a9260e3a683b8c274d
PremTeja777 <115568172+PremTeja777@users.noreply.github.com>
2022-10-30 23:28:26 +0530

    Add files via upload
    Full Stack Java development course First assignment

---
+++ 2.Onetofour.java
@@ -0,0 +1,31 @@
+package mypc.pc;
+
+public class Onetofour {
+
+	public static void main(String[] args) 
+	{
+		
+		int n=4;
+		for(int i=0; i<n; i++)
+		{
+			System.out.print(n-3);
+		}
+		System.out.println();
+		for(int i=0; i<n; i++)
+		{
+			System.out.print(n-2);
+		}
+		System.out.println();
+		for(int i=0; i<n; i++)
+		{
+			System.out.print(n-1);
+		}
+		System.out.println();
+		for(int i=0; i<n; i++)
+		{
+			System.out.print(n);
+			
+		}
+	}
+
+}
